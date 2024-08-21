445c24d29391a4ce5dee0277c23d8a5fe50e6dba
m1ca000 <164351016+m1ca000@users.noreply.github.com>
2024-08-21 15:00:46 -0300

    fix: Arreglo en el cors
    Supuestamente ahora deberia de funcionar el cors en el front

--- Back/Controllers/user.js
+++ Back/Controllers/user.js
@@ -9,20 +9,23 @@ const client = new Client(config);
 await client.connect();
 
 const register = async (req, res) => {
-    console.log("register user")
-    await client.connect();
-    console.log("conectado")
+    console.log("register user");
+    console.log("conectado");
     const { email, username, password } = req.body;
     console.log("body", req.body)
     try {
+        let emailUsed = true;
+        let usernameUsed = true;
         const emailQuery = 'SELECT "email" FROM "user" WHERE "email" = $1';
         const emailValues = [email];
         const result = await client.query(emailQuery, emailValues);
         if (result.rows[0] == null){
+            emailUsed = false;
             const userQuery = 'SELECT "username" FROM "user" WHERE "username" = $1'
             const userValues = [username];
             const result2 = await client.query(userQuery, userValues);
             if(result2.rows[0] == null){
+                usernameUsed = false;
                 const saltRounds = 10;
                 const hashedPassword = await bcrypt.hash(password, saltRounds);
 
@@ -30,17 +33,21 @@ const register = async (req, res) => {
                 const values = [email, username, hashedPassword];
                 await client.query(query, values);
                 await client.end();
-                res.status(201).json({ error: 'Usuario registrado con éxito'});
+                res.status(201).json({ message: 'Usuario registrado con éxito'});
             }
             else{
+                usernameUsed = true;
                 res.status(400).json({ error: 'Este nombre ya se encuentra en uso' });
                 return;
             }
         }
         else {
+            emailUsed = true;
             res.status(400).json({ error: 'Este email ya se encuentra en uso' });
             return;
         }
+        return emailUsed;
+        return usernameUsed;
     } 
     catch (err) {
         console.error(err);
@@ -70,8 +77,6 @@ const login = async (req, res) => {
                 const updateQuery = 'UPDATE "user" SET "login_code" = $1, "login_code_timestamp" = $2 WHERE "email" = $3';
                 const updateValues = [code, timestamp, email];
                 await client.query(updateQuery, updateValues);
-                console.log('EMAIL_USER:', process.env.EMAIL_USER);
-                console.log('EMAIL_PASSWORD:', process.env.EMAIL_PASSWORD);
 
                 const transporter = nodemailer.createTransport({
                 host: 'smtp.gmail.com',
@@ -90,20 +95,20 @@ const login = async (req, res) => {
                     text: `Your login code is: ${code}`,
                 };
                 await transporter.sendMail(mailOptions);
-                res.status(200).send('Sesión iniciada con éxito. Verifica tu email para obtener el código de login');
+                res.status(200).json({ message: 'Sesión iniciada con éxito. Verifica tu email para obtener el código de login' });
             }
             else {
-                res.status(401).send('La contraseña no coincide con el email');
+                res.status(401).json({ error: 'La contraseña no coincide con el email' });
             }
         } 
         else {
-          res.status(404).send('No se ha encontrado una cuenta con ese email');
+          res.status(404).json({ error: 'No se ha encontrado una cuenta con ese email' });
         }
     }
     catch (err) {
         console.error(err);
         await client.end();
-        res.status(500).send('Error al iniciar sesión');
+        res.status(500).json({ error: 'Error al iniciar sesión' });
     }
 };
 
@@ -121,16 +126,16 @@ const verifyCode = async (req, res) => {
         const storedTimestamp = result.rows[0].login_code_timestamp;
   
         if (storedCode == code && new Date().getTime() - storedTimestamp < 60000) {
-          res.status(200).send('Código de login válido');
+          res.status(200).json({ message: 'Código de login válido' });
         } else {
-          res.status(401).send('Código de login inválido o ha expirado');
+          res.status(401).json({ error: 'Código de login inválido o ha expirado' });
         }
       } else {
-        res.status(404).send('No se ha encontrado una cuenta con ese email');
+        res.status(404).json({ error: 'No se ha encontrado una cuenta con ese email' });
       }
     } catch (err) {
       console.error(err);
-      res.status(500).send('Error al verificar código de login');
+      res.status(500).json({ error: 'Error al verificar código de login' });
     } finally {
       await client.end();
     }
@@ -152,21 +157,41 @@ const updatePassword = async (req, res) => {
             await client.query(queryPass, valuesPass);
             console.log("body", req.body)
             await client.end();
-            res.status(201).send('Contraseña modificada con exito');
+            res.status(201).json({ message: 'Contraseña modificada con exito' });
         }
     }
     catch {
         console.error(err);
         await client.end();
-        res.status(500).send('Error al modificar contraseña');
+        res.status(500).json({ error:'Error al modificar contraseña'});
     }
 };
+
+const userGroup = async (req, res) => {
+    console.log("User Group");
+    await client.connect();
+    console.log("Conectado");
+    const { group, email} = req.body;
+    try {
+        const query = 'UPDATE "user" SET "group" = $1 WHERE "email" = $2';
+        const values = [group, email];
+        await client.query(query, values);
+        await client.end();
+        res.status(201).json({ message: 'Union al grupo exitosa' });
+    }
+    catch {
+        console.error(err);
+        await client.end();
+        res.status(500).json({ error: 'Error al unirse al grupo' });
+    }
+}
 
 const user = {
     register,
     login,
     verifyCode,
     updatePassword,
+    userGroup,
 };
 
 export default user;
--- Back/index.js
+++ Back/index.js
@@ -4,11 +4,6 @@ import cors from 'cors'
 const app = express()
 const PORT = 8000
 
-app.use(cors({
-    origin: '*',
-    credentials: true
-  }));
-
 // Middlewares
 app.use(cors());
 app.use(express.json());
@@ -29,5 +24,6 @@ import user from "./Controllers/user.js";
 //User
 app.post('/api/register', user.register)
 app.post('/api/login', user.login)
-app.post('/api/updatePassword', user.updatePassword)
+app.put('/api/updatePassword', user.updatePassword)
 app.post('/api/verifyCode', user.verifyCode)
+app.put('/api/userGroup', user.userGroup)
