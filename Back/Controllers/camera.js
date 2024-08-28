79d7c322c20bce560ecc928c16259078f4cd5864
m1ca000 <164351016+m1ca000@users.noreply.github.com>
2024-08-28 15:32:48 -0300

    fix: Carpeta Config creada
    Cree una carpeta para la configuraciones y que todo este más organizado

--- Back/Controllers/camera.js
+++ Back/Controllers/camera.js
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import {config} from './db.js';
+import {config} from '../Config/db.js';
 import pkg from 'pg';
 import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 const {Client} = pkg;
 const client = new Client(config);
-import cloudinary from './CloudConfig.js'
+import cloudinary from '../Config/CloudConfig.js'
 
 const camera = {
 };
