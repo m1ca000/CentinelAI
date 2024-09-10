4d4f1b31f0a2191c31f5f50ba398a7edf85b1913
FedericoGaubeca <164924960+FedericoGaubeca@users.noreply.github.com>
2024-09-10 10:03:09 -0300

    feat: Orden con las carpetas y slide animation entre las paginas del dashboard

--- Pagina_Web/Dashboard/Dashboard.js
+++ Pagina_Web/Dashboard/Dashboard.js
@@ -1,15 +1,36 @@
 const items = document.querySelectorAll('.upper-bar li');
 const blocks = document.querySelectorAll('.block');
+let activeBlock = document.querySelector('.block.active');
 
-// Handle menu click
 items.forEach(item => {
     item.addEventListener('click', function() {
         items.forEach(i => i.classList.remove('active'));
         this.classList.add('active');
 
-        blocks.forEach(block => block.classList.remove('active'));
-
         const target = this.getAttribute('data-target');
-        document.querySelector(`.${target}`).classList.add('active');
+        const newBlock = document.querySelector(`.${target}`);
+
+        if (activeBlock !== newBlock) {
+            const direction = (Array.from(blocks).indexOf(newBlock) < Array.from(blocks).indexOf(activeBlock)) ? 'left' : 'right';
+            
+            activeBlock.classList.add(`slide-out-${direction}`);
+            activeBlock.style.opacity = '0';
+
+            newBlock.style.display = 'block';
+            newBlock.classList.add(`slide-in-${direction}`);
+            newBlock.style.opacity = '0';
+
+            setTimeout(() => {
+                newBlock.classList.remove(`slide-in-${direction}`);
+                newBlock.style.opacity = '1';
+
+                activeBlock.classList.remove(`active`, `slide-out-${direction}`);
+                activeBlock.style.display = 'none';
+                activeBlock.style.opacity = '0';
+
+                activeBlock = newBlock;
+                activeBlock.classList.add('active');
+            }, 500); // Duration should match the CSS transition time
+        }
     });
 });
